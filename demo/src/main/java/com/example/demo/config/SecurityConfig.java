df4b62cb3ae7402b67d8171aaa6fd870efa1dbeb
jyuen <jyuen@JYUEN-W1.telecomsys.com>
2019-08-04 09:33:05 -0700

    Add changes for basic authentication

---
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity(debug=true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
+	@Override
+    protected void configure(AuthenticationManagerBuilder managerBuilder) throws Exception{
+        managerBuilder.inMemoryAuthentication()
+                .withUser("joe").password("1234").roles("USER").and()
+                .withUser("admin").password("{noop}admin123").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic().and().authorizeRequests()
+                //.antMatchers("/addItem","/delete").hasRole("ADMIN")
+                .antMatchers("/inventory").hasRole("USER")
+                .and().csrf().disable().headers().frameOptions().disable();
+    }
+}
